69ad1eafbe453ed4f85ed10d08b85f68091c397e
晏瑞珑 <yanruilong@yanruilongdeMacBook-Air.local>
2018-04-24 21:40:30 +0800

    init project

--- src/main/java/com/ecut/yrl/ipinche/controller/mainController.java
+++ src/main/java/com/ecut/yrl/ipinche/controller/mainController.java
@@ -3,6 +3,8 @@ package com.ecut.yrl.ipinche.controller;
 import com.ecut.yrl.ipinche.dataobject.User;
 import com.ecut.yrl.ipinche.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class mainController {
+    private  final Logger logger = LoggerFactory.getLogger(mainController.class);
+
     @Autowired
     UserRepository userRepository;
     @GetMapping("/list")
@@ -19,6 +23,7 @@ public class mainController {
         User user = userRepository.findOneById("1");
         String name = user.getName();
         String openid = user.getOpenid();
+        logger.info("test log");
         return name+openid;
     }
 }
